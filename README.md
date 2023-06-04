8c7727688c77d37639e196dd67e516463e135eba
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2023-06-05 05:26:42 +0530

    05june done

--- README.md
+++ README.md
@@ -1,44 +1,49 @@
 # GFG Problem Of The Day
 
-## Today - 4 June 2023
-### Que - Reversing the equation
+## Today - 5 June 2023
+### Que - Find the Closest Element in BST
 
-[Question Link](https://practice.geeksforgeeks.org/problems/reversing-the-equation2205/1)
+[Question Link](https://practice.geeksforgeeks.org/problems/find-the-closest-element-in-bst/1)
 
 ### My approach
-- Use two stacks to store the numbers and operators.
-- Update the stacks as new numbers and operators are encountered.
-- Implement a top-to-bottom traversal of the stacks to print the numbers and operators alternatively.
+- Simple. traversal in binary search tree and find min different key with relative to `K` .
+
 
 ### Code (c++)
 ```
 class Solution
 {
-  public:
-    string reverseEqn (string str)
+    public:
+
+    void check(struct Node *node, int k, int &md,int &md_key)
+    {
+        if (node == NULL)
+            return ;
+     
+        if (node->data == k)
         {
-            stack<string> num;
-            stack<char> op;
-            string s = "";
-            for(auto i: str){
-                if(i<'0' || i>'9'){
-                    num.push(s);
-                    op.push(i);
-                    s = "";
-                }
-                else
-                s += i;
-            }
-            num.push(s);
-            s = "";
-            while(!op.empty()){
-                s += num.top();
-                s += op.top();
-                num.pop();
-                op.pop();
-            }
-            s += num.top();
-            return s;
+            md_key = k;
+            return;
         }
+     
+        if (md > abs(node->data - k))
+        {
+            md = abs(node->data - k);
+            md_key = node->data;
+        }
+     
+        if (k < node->data)
+            check(node->left, k, md, md_key);
+        else
+            check(node->right, k, md, md_key);
+    }
+     
+    int minDiff(Node *root, int k)
+    {
+        int md = INT_MAX, md_key = -1;
+        check(root, k, md, md_key);
+     
+        return abs(md_key-k);
+    }
 };
 ```
---
+++ june-2023/05june.cpp
@@ -0,0 +1,164 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+// } Driver Code Ends
+/*
+Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+    public:
+
+    void check(struct Node *node, int k, int &md,int &md_key)
+    {
+        if (node == NULL)
+            return ;
+     
+        if (node->data == k)
+        {
+            md_key = k;
+            return;
+        }
+     
+        if (md > abs(node->data - k))
+        {
+            md = abs(node->data - k);
+            md_key = node->data;
+        }
+     
+        if (k < node->data)
+            check(node->left, k, md, md_key);
+        else
+            check(node->right, k, md, md_key);
+    }
+     
+    int minDiff(Node *root, int k)
+    {
+        int md = INT_MAX, md_key = -1;
+        check(root, k, md, md_key);
+     
+        return abs(md_key-k);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+
+
+int main() {
+   
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+        string s; 
+       getline(cin, s);
+       Node* root1 = buildTree(s);
+
+       getline(cin, s);
+       int k = stoi(s);
+       // getline(cin, s);
+
+        Solution ob;
+       cout << ob.minDiff(root1, k);
+        cout << endl;
+       //cout<<"~"<<endl;
+   }
+   return 0;
+}
+
+// } Driver Code Ends
