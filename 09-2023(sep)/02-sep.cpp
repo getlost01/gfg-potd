779f0ef9a905fda03a7f04e812b31c8621ea7fff
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2023-09-02 14:41:46 +0530

    02 sep done

---
+++ 09-2023(sep)/02-sep.cpp
@@ -0,0 +1,156 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printInorder(Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    printInorder(node->left);
+    cout << node->data << " ";
+    printInorder(node->right);
+}
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    int getCount(Node *root, int k)
+    {
+        queue<pair<Node*, int>> q;
+        q.push({root, 1});
+        int cnt = 0;
+        
+        while (!q.empty() && k > 0){
+            auto node = q.front().first;
+            int level = q.front().second;
+            q.pop();
+            
+            if (!node->left && !node->right){
+                if (level > k)
+                    return cnt;
+                    
+                ++cnt;
+                k -= level;
+            }else{
+                if (node->left)
+                    q.push({node->left, level + 1});
+                if (node->right)
+                    q.push({node->right, level + 1});
+            }
+        }
+        
+        return cnt;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    cin.ignore();
+    while (t--)
+    {
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        int k;
+        cin >> k;
+        cin.ignore();
+        Solution obj;
+        int res = obj.getCount(root, k);
+        cout << res << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
