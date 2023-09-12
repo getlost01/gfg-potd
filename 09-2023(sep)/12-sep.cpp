720859769aa34c3a16340c1f9d21b95780556ff9
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2023-09-12 11:17:54 +0530

    12 sep done

---
+++ 09-2023(sep)/12-sep.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int isPerfectNumber(long long n) {
+        if(n == 1)
+            return 0;
+        long long sum = 1;
+        for(int i = 2; i<=sqrt(n) ; ++i){
+            if(n%i == 0){
+                sum += i;
+                if(n%i != i)
+                    sum += n/i;
+            }
+        }
+        return sum == n;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.isPerfectNumber(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
