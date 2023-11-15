ce74e02e4dbddbb5671b58df9dfa1932f985da11
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2023-11-15 11:08:30 +0530

    15 nov done

---
+++ 11-2023(nov)/15-nov.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    int distSubSeq(string s)
+    {
+        unordered_map<char, int> last;
+        int count = 1;
+
+        for (auto ch : s) {
+            int newCount = 2 * count;
+
+            if (last.find(ch) != last.end())
+                newCount -= last[ch];
+
+            last[ch] = count;
+            count = newCount;
+        }
+        return count;
+    }
+
+    string betterString(string s1, string s2) {
+        int distSubSeq1 = distSubSeq(s1);
+        int distSubSeq2 = distSubSeq(s2);
+
+        return distSubSeq2 > distSubSeq1 ? s2 : s1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str1, str2;
+        cin >> str1 >> str2;
+        Solution obj;
+        string ans = obj.betterString(str1, str2);
+        cout << ans << "\n";
+    }
+}
+
+// } Driver Code Ends
