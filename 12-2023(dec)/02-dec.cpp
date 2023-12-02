e87c03288ecc48a351c8b980a4b7a04286698dba
Saransh Bangar <saranshbangad@gmail.com>
2023-12-02 09:26:15 +0530

    Create 02-dec.cpp
    Added GFG POTD solution in C++

---
+++ 12-2023(dec)/02-dec.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int isRepresentingBST(int arr[], int N)
+    {
+        for (int i=0;i<N-1;i++)
+        {
+            if (arr[i]<arr[i+1])
+                continue;
+            else return 0;
+        }
+        return 1;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int arr[N];
+        for(int i=0;i<N;i++)cin>>arr[i];
+        Solution ob;
+        cout<<ob.isRepresentingBST(arr,N)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
