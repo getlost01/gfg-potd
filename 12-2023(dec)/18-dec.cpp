2377e125b1784ccda3647aeb449deda918712163
Saransh Bangar <saranshbangad@gmail.com>
2023-12-18 07:59:03 +0530

    Create 18-dec.cpp

---
+++ 12-2023(dec)/18-dec.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int gameOfXor(int N , int A[])
+    {
+        int ans=0;
+        for (int i = 0; i < N; ++i)
+        {
+            int num=(i+1)*(N-i);
+            if (num%2==1)
+                ans^=A[i];
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int A[N];
+        for(int i=0 ; i<N ; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.gameOfXor(N,A) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
