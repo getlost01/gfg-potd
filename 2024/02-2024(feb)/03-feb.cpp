a78971227520b88c2a75353ffc46e1ef55c9bff6
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2024-02-03 00:31:44 +0530

    03 feb done

---
+++ 2024/02-2024(feb)/03-feb.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+// Program to check if linked list is empty or not
+#include<iostream>
+using namespace std;
+const long long unsigned int MOD = 1000000007;
+
+/* Link list Node */
+struct Node
+{
+    bool data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    long long unsigned int decimalValue(Node *head)
+    {
+        long long unsigned int out = 0, mod = 1e9 + 7;
+        Node* temp = head;
+
+        while (temp) {
+            out = (out << 1) % mod;
+            out = (out + temp->data) % mod;
+            temp = temp->next;
+        }
+
+        return out;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+void append(struct Node** head_ref, struct Node **tail_ref, bool new_data)
+{
+
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+
+bool isEmpty(struct Node *head);
+
+/* Driver program to test above function*/
+int main()
+{
+    bool l;
+    int i, n, T;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+        Solution obj;
+        cout << obj.decimalValue(head) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
