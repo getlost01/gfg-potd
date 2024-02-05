a8f19351891f4600ccaf30e6d35be72a9bb30c46
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2024-02-05 17:56:37 +0530

    05 feb done

--- 2024/02-2024(feb)/05 - Sorted insert for circular linked list.md
+++ 2024/02-2024(feb)/05 - Sorted insert for circular linked list.md
@@ -1,7 +1,4 @@
-## GFG Problem Of The Day
-
-### Today - 05 February 2024
-### Que - Sorted insert for circular linked list
+## 05. Sorted insert for circular linked list
 The problem can be found at the following link: [Question Link](https://www.geeksforgeeks.org/problems/sorted-insert-for-circular-linked-list/1)
 
 ### My Approach
@@ -53,13 +50,6 @@ public:
 ```
 
 ### Contribution and Support
-
-I always encourage contributors to participate in the discussion forum for this repository.
-
-If you have a better solution or any queries / discussions related to the `Problem of the Day` solution, please visit our [discussion section](https://github.com/getlost01/gfg-potd/discussions). We welcome your input and aim to foster a collaborative learning environment.
+For discussions, questions, or doubts related to this solution, please visit our [discussion section](https://github.com/getlost01/gfg-potd/discussions). We welcome your input and aim to foster a collaborative learning environment.
 
 If you find this solution helpful, consider supporting us by giving a `⭐ star` to the [getlost01/gfg-potd](https://github.com/getlost01/gfg-potd) repository.
-
-![Total number of repository visitors](https://komarev.com/ghpvc/?username=gl01potdgfg&color=blue&&label=Visitors)
-![](https://hit.yhype.me/github/profile?user_id=79409258)
-
---
+++ 2024/02-2024(feb)/05-feb.cpp
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+/* structure for a Node */
+struct Node
+{
+  int data;
+  struct Node *next;
+  
+  Node(int x){
+      data = x;
+      next = NULL;
+  }
+  
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    Node* sortedInsert(Node* head, int data) {
+        Node* newNode = new Node(data);
+
+        if (!head) {
+            newNode->next = newNode;
+            return newNode;
+        }
+
+        Node* curr = head;
+        Node* next = curr->next;
+
+        if (data <= curr->data) {
+            newNode->next = head;
+            while (curr->next != head)
+                curr = curr->next;
+
+            curr->next = newNode;
+            return newNode;
+        }
+
+        while (next != head && data > next->data) {
+            curr = next;
+            next = next->next;
+        }
+
+        curr->next = newNode;
+        newNode->next = next;
+
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *start)
+{
+  struct Node *temp;
+
+  if(start != NULL)
+  {
+    temp = start;
+    do { printf("%d ", temp->data);
+      temp = temp->next;
+    } while(temp != start);
+    printf("\n");
+  }
+}
+/* Driver program to test above functions */
+int main()
+{
+int t,n,x;
+scanf("%d",&t);
+int arr;
+  while(t--){
+  scanf("%d",&n);
+  int list_size, i;
+  
+  /* start with empty linked list */
+  struct Node *start = NULL;
+  struct Node *temp,*r;
+  
+  
+    /* Create linked list from the array arr[].
+    Created linked list will be 1->2->11->56->12 */
+    if(n>0){
+      scanf("%d",&arr);
+    
+    temp = new Node(arr);
+    start=temp;
+    r=start;
+    }
+    for (i = 0; i<n-1; i++)
+    {
+        scanf("%d",&arr);
+        temp = new Node(arr);
+        r->next=temp;
+        r=r->next;
+    }
+    
+    if(n>0)
+    temp->next=start;
+ 
+    scanf("%d",&x);
+    Solution ob;
+    start = ob.sortedInsert(start,x);
+    printList(start);
+    r=start;
+    while(r!=start->next)
+    {
+
+      temp=start;
+      start=start->next;
+      free(temp);
+    }
+    free(start);
+}
+  return 0;
+}
+
+// } Driver Code Ends
