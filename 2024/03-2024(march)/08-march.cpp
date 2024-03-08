1f524ed57a746eb1c72ef249fd565f2436d3a1d9
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2024-03-08 09:37:21 +0530

    08 march done

---
+++ 2024/03-2024(march)/08-march.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    bool sameFreq(string s)
+    {
+        unordered_map<char,int> cnt;
+        for(auto i: s)
+            cnt[i]++;
+        
+        int nin, nax;
+        nin = nax = cnt[s[0]];
+        
+        for(auto i: cnt){
+            nin = min(nin, i.second);
+            nax = max(nax, i.second);
+            if(nax - nin > 1)
+                return 0;
+        }
+        
+        int cnin = 0;
+        for(auto i: cnt)
+            if(i.second == nin)
+                ++cnin;
+            
+        return cnt.size() - cnin <= 1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.sameFreq(s)<<endl;
+    }
+}
+
+
+
+// } Driver Code Ends
