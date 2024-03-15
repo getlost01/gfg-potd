abf61b4418d29dc2850f0092ccd204ddaa635534
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2024-03-15 11:16:15 +0530

    15 march done

---
+++ 2024/03-2024(march)/15-march.cpp
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+/* Function to print linked list */
+
+
+void append(struct Node** headRef, struct Node** tailRef, int newData)
+{
+	struct Node* new_node = new Node(newData);
+	struct Node *last = *headRef;
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    *tailRef = *headRef;
+	    return;
+	}
+	
+	(*tailRef) -> next = new_node;
+	*tailRef = (*tailRef) -> next;
+}
+
+// A utility function to print a linked list
+void printList(Node *head)
+{
+	while (head != NULL)
+	{
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    Node* reverse(Node* head) {
+        Node* prev = nullptr;
+        Node* curr = head;
+        while (curr) {
+            Node* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    void sort(Node **head)
+    {
+        if (!(*head) || !(*head)->next)
+            return;
+
+        Node *asc = (*head), *des= (*head)->next;
+        Node* revStart = des;
+        
+        while (des && des->next) {
+            asc->next = des->next;
+            asc = asc->next;
+            des->next = asc->next;
+            des = des->next;
+        }
+        
+        Node* temp = reverse(revStart);
+        asc->next = temp;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int test;
+	cin>>test;
+	while(test--)
+	{
+	    struct Node* head = NULL;
+	    struct Node* tail = NULL;
+	    int n, k;
+	    cin>>n;
+	    for(int i=0; i<n ;i++){
+	        cin>>k;
+	        append(&head, &tail, k);
+	    }
+	    Solution ob;
+	    ob.sort(&head);
+	    printList(head);
+	}
+	return 0;
+}
+
+// } Driver Code Ends
