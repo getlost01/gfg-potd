2a8d59ebd46da25e54d4b03f7b791a1c4b238c20
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2024-03-16 11:50:24 +0530

    16 march done

---
+++ 2024/03-2024(march)/16-march.cpp
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}*head;
+
+Node *findNode(Node* head, int search_for)
+{
+    Node* current = head;
+    while (current != NULL)
+    {
+        if (current->data == search_for)
+            break;
+        current = current->next;
+    }
+    return current;
+}
+
+
+void insert()
+{
+    int n,i,value;
+    Node *temp;
+    scanf("%d",&n);
+
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&value);
+        if(i==0)
+        {
+            head=new Node(value);
+            temp=head;
+            continue;
+        }
+        else
+        {
+            temp->next= new Node(value);
+            temp=temp->next;
+            temp->next=NULL;
+        }
+    }
+}
+
+/* Function to print linked list */
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    cout << endl;
+}
+
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    void deleteNode(Node *del_node) {
+        Node* next_node = del_node->next;
+        del_node->data = next_node->data;
+        del_node->next = next_node->next;
+        free(next_node);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Drier program to test above function*/
+int main(void)
+{
+    /* Start with the empty list */
+
+    int t,k,n,value;
+    
+    scanf("%d",&t);
+    while(t--)
+    {
+        insert();
+        scanf("%d",&k);
+        Node *del = findNode(head, k);
+        Solution ob;
+        if (del != NULL && del->next != NULL)
+        {
+            ob.deleteNode(del);
+        }
+        printList(head);
+    }
+    return(0);
+}
+
+
+
+// } Driver Code Ends
