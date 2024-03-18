8183ea8e77c28c5790315ba880b1f41610bbc58d
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2024-03-18 10:09:43 +0530

    18 march done

---
+++ 2024/03-2024(march)/18-march.cpp
@@ -0,0 +1,153 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    vector<int> levelOrder(Node* root) {
+        queue<Node*> q;
+        vector<int> out;
+        
+        if(root == nullptr)
+            return out;
+        
+        q.push(root);
+        
+        while(!q.empty()) {
+            auto front = q.front();
+            q.pop();
+            out.push_back(front->data);
+            
+            if(front->left)
+                q.push(front->left);
+            if(front->right)
+                q.push(front->right);
+        }
+        
+        return out;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Helper function to test mirror(). Given a binary
+   search tree, print out its data elements in
+   increasing sorted order.*/
+void inOrder(struct Node* node)
+{
+  if (node == NULL)
+    return;
+
+  inOrder(node->left);
+  printf("%d ", node->data);
+
+  inOrder(node->right);
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d ",&t);
+  while (t--)
+  {
+        string s;
+		getline(cin,s);
+		Node* root = buildTree(s);
+		Solution ob;
+        vector <int> res = ob.levelOrder(root);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+  }
+  return 0;
+}
+
+
+
+// } Driver Code Ends
