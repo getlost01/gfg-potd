a49b3f5cd2af1485b6c63d34ba40f6a2218816d6
Saransh Bangar <saranshbangad@gmail.com>
2024-07-03 22:54:56 +0530

    Create 01-july.cpp

---
+++ 2024/07-2024(july)/01-july.cpp
@@ -0,0 +1,137 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void push(struct Node **head_ref, int new_data) {
+
+    struct Node *new_node = new Node(new_data);
+
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void lvl(TreeNode *r) {
+    if (r == NULL)
+        return;
+    queue<TreeNode *> q;
+    q.push(r);
+    while (!q.empty()) {
+        TreeNode *j = q.front();
+        cout << j->data << " ";
+        q.pop();
+        if (j->left)
+            q.push(j->left);
+        if (j->right)
+            q.push(j->right);
+    }
+}
+
+void reverses(Node **head) {
+    Node *prev = NULL;
+    Node *cur = *head;
+    Node *nxt;
+    while (cur != NULL) {
+        nxt = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nxt;
+    }
+    *head = prev;
+}
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void convert(Node *head, TreeNode *&root) {
+        vector<int> arr;
+        Node* curr = head;
+        
+        while(curr) {
+            arr.push_back(curr->data);
+            curr = curr->next;
+        }
+        
+        buildTree(root, arr, 0);
+    }
+    
+  private:
+    void buildTree(TreeNode* &root, vector<int> &arr, int index) {
+        root = new TreeNode(arr[index]);
+        
+        int leftIndex = 2 * index + 1;
+        int rightIndex = 2 * index + 2;
+        
+        if(leftIndex < arr.size())
+            buildTree(root->left, arr, leftIndex);
+        else
+            return;
+        if(rightIndex < arr.size())
+            buildTree(root->right, arr, rightIndex);
+        else
+            return;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T, i, n, l, k;
+
+    cin >> T;
+
+    while (T--) {
+        struct Node *head = NULL;
+
+        cin >> n;
+        for (i = 1; i <= n; i++) {
+            cin >> l;
+            push(&head, l);
+        }
+        reverses(&head);
+        Node *prevhead = head;
+        TreeNode *root = NULL;
+        Solution obj;
+
+        obj.convert(head, root);
+        if (root == NULL) {
+            cout << "-1\n";
+        } else if (root == NULL && prevhead != NULL) {
+            cout << "-1\n";
+        } else {
+            lvl(root);
+            cout << endl;
+        }
+        // inorder(root);
+        getchar();
+    }
+    return 0;
+}
+
+// } Driver Code Ends
